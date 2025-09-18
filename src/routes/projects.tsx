e283b38f01e0adba68587979192cb398287228db
Grace <gxu660@aucklanduni.ac.nz>
2025-09-18 22:16:09 +1200

    Complete projects page layout with modular components

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -3,6 +3,7 @@ import { ProjectCard } from '@/components'
 import { allProjects } from '@/data/projects'
 import './styles/projects.css'
 import { useMobile } from '@/context/MobileContext'
+import ToTop from '@/components/ToTop'
 
 export const Route = createFileRoute('/projects')({
   component: RouteComponent,
@@ -10,11 +11,29 @@ export const Route = createFileRoute('/projects')({
 
 function RouteComponent() {
   const isMobile = useMobile();
+  const semesters = Object.keys(allProjects);
+
   return <>
+    <h1 className='header-container'>Projects</h1>
+    <div className='semester-buttons'>
+      {semesters.map(sem => (
+        <button className="semester-button"
+          key={sem}
+          onClick={() => {
+            const el = document.getElementById(sem);
+            if (el) {
+              el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+          }}
+        >
+          {sem}
+        </button>
+      ))}
+    </div>
     <section className="projects-section">
       
       {Object.entries(allProjects).map(([sem, projects]) => (
-        <div key={sem}>
+        <div key={sem} id={sem}>
           <h2 className={isMobile ? 'semester-title-mobile' : 'semester-title'}>{sem}</h2>
           <div className={isMobile ? "cards-container-mobile" : "cards-container"}>
             {projects.map((project, idx) => (
@@ -28,7 +47,11 @@ function RouteComponent() {
           </div>
         </div>
       ))}
+      <div style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '40px' }}>
+        <ToTop />
+      </div>
     </section>
+    
     
   </>
 
