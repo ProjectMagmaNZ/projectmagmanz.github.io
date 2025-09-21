cdefe0afe55cd96b47566af3c37b4eaeead2264c
Edmund Lu <edmundl0212@gmail.com>
2025-09-21 16:51:06 +1200

    Add buttons to contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -9,12 +9,36 @@ function RouteComponent() {
   return (
     <section className="contact-section">
       <h2 className="contact-h2">Please send us a message at:</h2>
-      <a
+      {/* <a
         href="mailto:projectmagmanz@gmail.com"
         className="contact-email"
       >
         projectmagmanz@gmail.com
+      </a> */}
+      
+      <br></br>
+
+      <center>
+      <a href="mailto:projectmagmanz@gmail.com">
+          <button className="contact-button">Our official email address</button>
       </a>
+      </center>
+
+      <br></br>
+
+      <br></br>
+
+      <br></br>
+
+      <h2 className="contact-h2">Or to sign up for our mailing list:</h2>
+
+      <br></br>
+
+      <center>
+      <a href="https://forms.gle/wMDQGo9Ews73hzHK6">
+          <button className="contact-button">Join us via this form</button>
+      </a>
+      </center>
     </section>
   );
 }
